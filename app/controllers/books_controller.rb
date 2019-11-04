c8e2a6b646ce100c138df13858211102ae094ff0
Bartek Wilczek <bartek.wilczek@toptal.com>
2019-11-04 20:43:18 +0100

    Add endpoint for creation/deletion

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,13 @@ class BooksController < ApplicationController
   def show
     render json: Book.find(params[:id])
   end
+
+  def create
+    render json: Book.create!(author: params[:author], title: params[:title])
+  end
+
+  def destroy
+    res = Book.delete(params[:id])
+    render json: {result: res}
+  end
 end
